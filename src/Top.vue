73d0f1ea866441737f6ff0d4acb8f27d413588ff
nai217 <syo13.sw29@gmail.com>
2020-06-24 19:09:14 +0900

    add tag

---
+++ src/Top.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="top">
+    <!-- TODO: v-forにする。href動的に生成する。 -->
+    <Tag tagName="this is tag"/>
+  </div>
+</template>
+
+<script>
+import Tag from './components/Tag.vue'
+
+export default {
+  name: 'Top',
+  components: {
+    Tag
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Tag.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="tag">
+    <h1>Hotタグ</h1>
+    <a href="#" class="btn-icon"><p class="tag-name"># {{ tagName }}</p></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  props: {
+    tagName: String
+  }
+}
+</script>
+
+<style scoped>
+.btn-icon {
+  display: inline-block;
+  max-width: 180px;
+  background-color: #FFF;
+  color: #7DC097;
+  text-decoration: none;
+  font-weight: bold;
+  border-radius: 50%;
+  border-color: #A7A7A7;
+  position: relative;
+  margin: 2px 4px;
+  border: solid 2px #A7A7A7;
+}
+
+.tag-name {
+  font-size: 16px;
+  padding: 8px 8px 8px 8px;
+}
+
+.btn-icon:before {
+  font-family: "FontAwesome";
+  position: absolute;
+
+}
+
+.btn-icon:hover {
+  opacity: 0.8;
+}
+</style>
