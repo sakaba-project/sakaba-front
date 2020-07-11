df532404ac2de1b3d4cea54a94a69ef948ac29ee
naoi217 <51935739+naoi217@users.noreply.github.com>
2020-07-11 18:50:54 +0900

    Front top tag (#16)
    * add tag
    
    * add top &vue-router
    
    * add reset css
    
    * 指摘の修正
    
    https://github.com/sakaba-project/sakaba-front/pull/16#discussion_r449882827
    
    https://github.com/sakaba-project/sakaba-front/pull/16#discussion_r449883038

---
+++ src/components/Tag.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tag">
+    <a href="#" class="btn-icon"><p class="tag-name">#{{ tagName }}</p></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  props: {
+    tagName: String
+  }
+}
+</script>
+
+<style scoped>
+.btn-icon {
+  display: inline-block;
+  max-width: 180px;
+  background-color: #FFF;
+  color: #7DC097;
+  text-decoration: none;
+  font-weight: bold;
+  border-radius: 3em;
+  border: solid 1px #A7A7A7;
+  position: relative;
+  margin: 2px 4px;
+}
+
+.tag-name {
+  font-size: 0.5em;
+  padding: 1em;
+  margin: 0;
+}
+
+.btn-icon:before {
+  position: absolute;
+}
+
+.btn-icon:hover {
+  opacity: 0.8;
+}
+</style>
---
+++ src/components/VoiceCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="voice-card">
+        <p class="voice-card-title">voice title</p>
+        <div class="voice-card-tags">
+            <Tag tagName="this is tag"/>
+        </div>
+        <p class="voice-card-message">aaaaaaa</p>
+  </div>
+</template>
+
+<script>
+import Tag from './Tag.vue'
+
+export default {
+    name: 'VoiceCard',
+    // props: {
+    //     title: String
+    //     tags: [String]
+    //     message: String
+    // }
+    components: {
+        Tag
+    }
+}
+</script>
+
+<style scoped>
+
+.voice-card {
+    padding: 0.5em 1em;
+    border-radius: 1em;
+    border: solid 2px #A7A7A7;
+}
+
+.voice-card-title {
+  margin: 0.5em 0em 0em;
+}
+
+.voice-card-tags {
+  margin: 0.5em 0em 0em;
+}
+
+.voice-card-message {
+  margin: 0.5em 0em 0em;
+}
+</style>
